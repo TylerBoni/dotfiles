e38e71eee36d56e4d04b8df55bf64dec8c664793
tylerboni <tboni@abonitech.com>
2024-03-12 23:34:43 -0700

    added install base

--- .local/bin/starti3.sh
+++ .local/bin/starti3.sh
@@ -1,5 +1,5 @@
-xmodmap -e 'remove Mod4 = Super_R'
-xmodmap -e 'add Mod3 = Super_R'
-xbindkeys
+#xmodmap -e 'remove Mod4 = Super_R'
+#xmodmap -e 'add Mod3 = Super_R'
+#xbindkeys
 cp ~/.config/x/.xinitrc.i3 ~/.xinitrc
 startx
