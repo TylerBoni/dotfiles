aacec5ed1453a6eba059991b5f04785ff9c88bc2
tylerboni <tboni@abonitech.com>
2024-03-25 19:03:34 -0700

    imporoved status notification

--- .config/notifications/status.sh
+++ .config/notifications/status.sh
@@ -1,8 +1,11 @@
-time_now=$(date +"%H:%M") # Current time
+# time_now=$(date +"%H:%M") # Current time
+# current time in 12 hour format
+time_now=$(date +"%I:%M %p") # Current time
 capacity=$(cat /sys/class/power_supply/BAT0/capacity)% # Battery capacity
+bat_status=$(cat /sys/class/power_supply/BAT0/status) # Battery status
 # pretty formatting of the notification
 #
-text="$time_now $capacity"
-notify-send -t 2000 $text 
+capacity=$capacity\ $bat_status
+notify-send -t 2000 "$time_now" "$capacity"
 
 
