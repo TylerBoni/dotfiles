dba36085d3c456d359b9dfb2589927229dcc9dbf
tylerboni <tboni@abonitech.com>
2024-03-25 18:38:46 -0700

    cleanup and hyprland additions

--- .local/bin/install_base.sh
+++
@@ -1,5 +0,0 @@
-sudo pacman -S --noconfirm --needed base-devel git wget neovim python-pip i3-wm xorg-xinit xorg-server xorg-xrandr ttf-hack-nerd alacritty polybar dmenu picom xclip network-manager-applet pulseaudio pulseaudio-bluetooth pulsemixer 
-
-#for bluetooth headphones (br-connection-profile-unavailable)
-pacmd load-module module-bluetooth-policy
-pacmd load-module module-bluetooth-discover
